39d81048420d4d8ec0b296eba4f3192bf52a6777
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-02-13 10:55:04 -0500

    corrected snake case; added contracts

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -82,7 +82,7 @@ export function useTableContract(): Contract | undefined {
 //
 // Function to retrieve svg from contract
 //
-export function CallContractStringifyReturn (
+export function callContractStringifyReturn (
   contract: Contract | undefined,
   method: string
 ): string | undefined {
